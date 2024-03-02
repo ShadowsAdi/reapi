f33a3f87fbae997ba662de180fe151c0889e1fc5
Adrian Cirstea <54354938+ShadowsAdi@users.noreply.github.com>
2024-03-02 23:07:44 +0200

    .

--- reapi/src/hook_callback.cpp
+++ reapi/src/hook_callback.cpp
@@ -242,6 +242,16 @@ void ExecuteServerStringCmd(IRehldsHook_ExecuteServerStringCmd* chain, const cha
 	callVoidForward(RH_ExecuteServerStringCmd, original, cmdName, cmdSrc, cmdSrc == src_client ? cl->GetId() + 1 : AMX_NULLENT);
 }
 
+void SV_SendServerinfo(IRehldsHook_SV_SendServerinfo* chain)
+{
+	auto original = [chain]()
+	{
+		chain->callNext();
+	};
+
+	callVoidForward(RH_SV_SendServerinfo, original);
+}
+
 /*
 * ReGameDLL functions
 */
